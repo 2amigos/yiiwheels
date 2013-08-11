6736463c57ef4f52ee4d58220f4a2de3454f5864
Antonio Ramirez <amigo.cobos@gmail.com>
2013-08-11 09:32:35 +0200

    fixed WhRelationalColumn

--- widgets/grid/WhRelationalColumn.php
+++ widgets/grid/WhRelationalColumn.php
@@ -70,6 +70,7 @@ class WhRelationalColumn extends TbDataColumn
         if (empty($this->url))
             $this->url = Yii::app()->getRequest()->requestUri;
 
+		$this->attachBehavior('ywplugin', array('class' => 'yiiwheels.behaviors.WhPlugin'));
         $this->registerClientScript();
     }
 
@@ -123,9 +124,13 @@ class WhRelationalColumn extends TbDataColumn
      */
     public function registerClientScript()
     {
-        Yii::app()->bootstrap->registerAssetCss('bootstrap-relational.css');
+		$path = __DIR__ . DIRECTORY_SEPARATOR . 'assets';
+		$assetsUrl = $this->getAssetsUrl($path);
+
         /** @var $cs CClientScript */
         $cs = Yii::app()->getClientScript();
+		$cs->registerCssFile($assetsUrl . '/css/bootstrap-relational.css');
+
         if ($this->afterAjaxUpdate !== null) {
             if ((!$this->afterAjaxUpdate instanceof CJavaScriptExpression) && strpos(
                     $this->afterAjaxUpdate,
